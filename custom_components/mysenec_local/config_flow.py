5a0b02a200d61ef522192ef10fe736304fcdc7ab
JanF <jfirlus@outlook.de>
2025-07-27 19:54:29 +0200

    Anpassung eingabe IP und Hostname

--- custom_components/mysenec_local/config_flow.py
+++ custom_components/mysenec_local/config_flow.py
@@ -9,5 +9,6 @@ class MySenecConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="MySenec Local", data=user_input)
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required("host"): str})
+            data_schema=vol.Schema({vol.Required("host", description={"suggested_value": "senec"}): str
+})
         )
