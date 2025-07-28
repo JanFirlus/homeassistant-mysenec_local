87e27e2accb18e7e3a4db91a7bd94315c67007ad
Jan <jfirlus@outlook.de>
2025-07-28 07:56:21 +0200

    Fix: HTTPS mit deaktivierter Zertifikatsprüfung

--- custom_components/mysenec_local/senec_api.py
+++ custom_components/mysenec_local/senec_api.py
@@ -1,8 +1,13 @@
 import aiohttp
+import ssl
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 class SenecLocalAPI:
     def __init__(self, host):
-        self._url = f"http://{host}/lala.cgi"
+        self._url = f"https://{host}/lala.cgi"
+        _LOGGER.debug("Verwende SENEC-URL: %s", self._url)
 
     async def async_get_data(self):
         payload = {
@@ -16,16 +21,23 @@ class SenecLocalAPI:
                 "LIVE_BAT_SOC": ""
             }
         }
-        async with aiohttp.ClientSession() as session:
-            async with session.post(self._url, json=payload, timeout=5) as resp:
-                result = await resp.json()
-                data = result["STATISTIC"]
-                return {
-                    "grid_import": data["LIVE_GRID_IMPORT"],
-                    "grid_export": data["LIVE_GRID_EXPORT"],
-                    "house_consumption": data["LIVE_HOUSE_CONSUMPTION"],
-                    "pv_power": data["LIVE_PV_GEN"],
-                    "battery_charge": data["LIVE_BAT_CHARGE"],
-                    "battery_discharge": data["LIVE_BAT_DISCHARGE"],
-                    "battery_soc": data["LIVE_BAT_SOC"]
-                }
+
+        sslcontext = ssl._create_unverified_context()
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(self._url, json=payload, timeout=5, ssl=sslcontext) as resp:
+                    result = await resp.json()
+                    data = result["STATISTIC"]
+                    return {
+                        "grid_import": data["LIVE_GRID_IMPORT"],
+                        "grid_export": data["LIVE_GRID_EXPORT"],
+                        "house_consumption": data["LIVE_HOUSE_CONSUMPTION"],
+                        "pv_power": data["LIVE_PV_GEN"],
+                        "battery_charge": data["LIVE_BAT_CHARGE"],
+                        "battery_discharge": data["LIVE_BAT_DISCHARGE"],
+                        "battery_soc": data["LIVE_BAT_SOC"]
+                    }
+        except Exception as e:
+            _LOGGER.error("SENEC API Fehler: %s", e)
+            return {}
