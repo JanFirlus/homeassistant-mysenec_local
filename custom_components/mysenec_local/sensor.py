71844fdcf79c8fe52e7bd2ca91e791bc6307fe17
JanF <jfirlus@outlook.de>
2025-07-29 06:57:04 +0200

    Sensor.py angepasst

--- custom_components/mysenec_local/sensor.py
+++ custom_components/mysenec_local/sensor.py
@@ -1,42 +1,39 @@
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import (
-    POWER_WATT,
-    PERCENTAGE,
-    DEVICE_CLASS_POWER,
-    DEVICE_CLASS_BATTERY,
-    STATE_CLASS_MEASUREMENT,
-)
-from .const import DOMAIN
 import logging
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import POWER_WATT, PERCENTAGE
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 SENSOR_TYPES = {
-    "pv_power": ("PV-Erzeugung", POWER_WATT, DEVICE_CLASS_POWER),
-    "grid_export": ("Einspeisung", POWER_WATT, DEVICE_CLASS_POWER),
-    "grid_import": ("Bezug", POWER_WATT, DEVICE_CLASS_POWER),
-    "house_consumption": ("Hausverbrauch", POWER_WATT, DEVICE_CLASS_POWER),
-    "battery_soc": ("Batterie SOC", PERCENTAGE, DEVICE_CLASS_BATTERY),
-    "battery_charge": ("Batterie lädt", POWER_WATT, DEVICE_CLASS_POWER),
-    "battery_discharge": ("Batterie entlädt", POWER_WATT, DEVICE_CLASS_POWER),
+    "pv_power": ("PV-Erzeugung", POWER_WATT),
+    "grid_export": ("Einspeisung", POWER_WATT),
+    "grid_import": ("Bezug", POWER_WATT),
+    "house_consumption": ("Hausverbrauch", POWER_WATT),
+    "battery_soc": ("Batterie SOC", PERCENTAGE),
+    "battery_charge": ("Batterie lädt", POWER_WATT),
+    "battery_discharge": ("Batterie entlädt", POWER_WATT),
 }
 
 async def async_setup_entry(hass, entry, async_add_entities):
+    _LOGGER.debug("Starte Sensor-Setup für MySenec Local...")
     coordinator = hass.data[DOMAIN][entry.entry_id]
     entities = []
 
-    for key, (name, unit, device_class) in SENSOR_TYPES.items():
-        entities.append(SenecSensor(coordinator, key, name, unit, device_class))
+    for key, (name, unit) in SENSOR_TYPES.items():
+        entities.append(SenecSensor(coordinator, key, name, unit))
+        _LOGGER.debug("Sensor erstellt: %s", key)
 
-    async_add_entities(entities, update_before_add=True)
+    async_add_entities(entities)
+    _LOGGER.debug("Alle Sensoren registriert.")
 
 class SenecSensor(SensorEntity):
-    def __init__(self, coordinator, key, name, unit, device_class):
+    def __init__(self, coordinator, key, name, unit):
         self._coordinator = coordinator
         self._key = key
         self._name = name
         self._unit = unit
-        self._device_class = device_class
+        self._attr_should_poll = False
 
     @property
     def name(self):
@@ -51,22 +48,13 @@ class SenecSensor(SensorEntity):
         return self._unit
 
     @property
-    def native_value(self):
+    def state(self):
+        if not self._coordinator.data:
+            _LOGGER.warning("Keine Daten im Coordinator für %s", self._key)
+            return None
         value = self._coordinator.data.get(self._key)
-        _LOGGER.debug("Sensor %s hat Wert: %s", self._key, value)
+        _LOGGER.debug("Sensor %s Wert: %s", self._key, value)
         return value
-
-    @property
-    def state_class(self):
-        return STATE_CLASS_MEASUREMENT
-
-    @property
-    def device_class(self):
-        return self._device_class
-
-    @property
-    def should_poll(self):
-        return False
 
     async def async_update(self):
         await self._coordinator.async_request_refresh()
