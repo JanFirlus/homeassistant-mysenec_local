1d91dbbda606b5042e35d8c76edfd1fc3720a8f7
JanF <jfirlus@outlook.de>
2025-07-29 07:02:49 +0200

    Fixed init

--- custom_components/mysenec_local/__init__.py
+++ custom_components/mysenec_local/__init__.py
@@ -20,11 +20,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setups(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     for platform in PLATFORMS:
